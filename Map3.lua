24e275070464d732ce80bb474a26498fd6450686
jkancc111 <jkancc3@gmail.com>
2025-04-04 19:24:03 +0700

    Create Map3.lua

---
+++ Map3.lua
@@ -0,0 +1,326 @@
+return {
+    Worlds = {
+        {
+            Name = "200 | Void Spawn",
+            TeleportPart = CFrame.new(-2850.572998046875, 14.056390762329102, -8504.443359375),
+            FarmPart = CFrame.new(-2794.620849609375, 13.841465950012207, -8503.1103515625)
+        },
+        {
+            Name = "201 | Prison Block",
+            TeleportPart = CFrame.new(-2698.324951171875, 14.385641098022461, -8507.1904296875),
+            FarmPart = CFrame.new(-2631.832763671875, 13.841465950012207, -8503.982421875)
+        },
+        {
+            Name = "202 | Prison Cafeteria",
+            TeleportPart = CFrame.new(-2535.024169921875, 14.353550910949707, -8507.630859375),
+            FarmPart = CFrame.new(-2467.959716796875, 13.841465950012207, -8504.15234375)
+        },
+        {
+            Name = "203 | Prison Yard",
+            TeleportPart = CFrame.new(-2372.123779296875, 14.317835807800293, -8506.9921875),
+            FarmPart = CFrame.new(-2305.603759765625, 14.591465950012207, -8505.9482421875)
+        },
+        {
+            Name = "204 | Prison HQ",
+            TeleportPart = CFrame.new(-2208.323974609375, 13.62100601196289, -8504.15625),
+            FarmPart = CFrame.new(-2141.937255859375, 14.591465950012207, -8505.8544921875)
+        },
+        {
+            Name = "205 | Beach Island",
+            TeleportPart = CFrame.new(-2629.7158203125, 231.2174072265625, -12963.6396484375),
+            FarmPart = CFrame.new(-2684.145263671875, 230.8227996826172, -12964.6865234375)
+        },
+        {
+            Name = "206 | Ocean Island",
+            TeleportPart = CFrame.new(-3233.224365234375, 291.1986999511719, -12969.77734375),
+            FarmPart = CFrame.new(-3304.645263671875, 290.82281494140625, -12966.9365234375)
+        },
+        {
+            Name = "207 | Tiki Island",
+            TeleportPart = CFrame.new(-3826.1015625, 411.3309326171875, -12969.77734375),
+            FarmPart = CFrame.new(-3894.145263671875, 409.9169006347656, -12968.7373046875)
+        },
+        {
+            Name = "208 | Jungle Island",
+            TeleportPart = CFrame.new(-4248.77685546875, 592.3612670898438, -12942.4501953125),
+            FarmPart = CFrame.new(-4295.79541015625, 591.7673950195312, -12942.1865234375)
+        },
+        {
+            Name = "209 | Volcano Island",
+            TeleportPart = CFrame.new(-4712.11767578125, 703.6817016601562, -12955.0283203125),
+            FarmPart = CFrame.new(-4758.54541015625, 702.367431640625, -12955.38671875)
+        },
+        {
+            Name = "210 | Hacker Matrix",
+            TeleportPart = CFrame.new(-2610.189208984375, 221.7504425048828, -22098.1875),
+            FarmPart = CFrame.new(-2685.16845703125, 221.42428588867188, -22099.7421875)
+        },
+        {
+            Name = "211 | Hacker Fortress",
+            TeleportPart = CFrame.new(-2381.5166015625, 197.1315155029297, -22097.1875),
+            FarmPart = CFrame.new(-2308.0126953125, 197.35687255859375, -22097.5078125)
+        },
+        {
+            Name = "212 | Hacker Cave",
+            TeleportPart = CFrame.new(-2688.826904296875, 196.66514587402344, -21800.216796875),
+            FarmPart = CFrame.new(-2689.771484375, 197.10687255859375, -21726.46875)
+        },
+        {
+            Name = "213 | Hacker Lab",
+            TeleportPart = CFrame.new(-2988.027587890625, 195.02821350097656, -22092.69921875),
+            FarmPart = CFrame.new(-3062.242431640625, 195.48187255859375, -22094.216796875)
+        },
+        {
+            Name = "214 | Hacker Mainframe",
+            TeleportPart = CFrame.new(-2686.27978515625, 194.35491943359375, -22400.6171875),
+            FarmPart = CFrame.new(-2686.020751953125, 194.93186950683594, -22473.296875)
+        },
+        {
+            Name = "215 | Dirt Village",
+            TeleportPart = CFrame.new(713.1748046875, 13.715641021728516, -14306.9501953125),
+            FarmPart = CFrame.new(779.667236328125, 13.841465950012207, -14305.857421875)
+        },
+        {
+            Name = "216 | Stone Forts",
+            TeleportPart = CFrame.new(875.4248046875, 13.696640968322754, -14306.9501953125),
+            FarmPart = CFrame.new(941.917236328125, 13.841465950012207, -14305.857421875)
+        },
+        {
+            Name = "217 | Silver City",
+            TeleportPart = CFrame.new(1037.2998046875, 13.704641342163086, -14306.9501953125),
+            FarmPart = CFrame.new(1103.792236328125, 13.841465950012207, -14305.857421875)
+        },
+        {
+            Name = "218 | Golden Metropolis",
+            TeleportPart = CFrame.new(1198.7998046875, 13.698640823364258, -14306.9501953125),
+            FarmPart = CFrame.new(1265.292236328125, 13.841465950012207, -14305.857421875)
+        },
+        {
+            Name = "219 | Diamond Mega City",
+            TeleportPart = CFrame.new(1360.7998046875, 13.286641120910645, -14306.9501953125),
+            FarmPart = CFrame.new(1427.292236328125, 13.841465950012207, -14305.857421875)
+        },
+        {
+            Name = "220 | Kawaii Tokyo",
+            TeleportPart = CFrame.new(713.1748046875, 13.715641021728516, -36306.94921875),
+            FarmPart = CFrame.new(779.667236328125, 13.841465950012207, -36305.859375)
+        },
+        {
+            Name = "221 | Kawaii Village",
+            TeleportPart = CFrame.new(875.4248046875, 13.696640968322754, -36306.94921875),
+            FarmPart = CFrame.new(941.917236328125, 13.841465950012207, -36305.859375)
+        },
+        {
+            Name = "222 | Kawaii Grove",
+            TeleportPart = CFrame.new(1037.2998046875, 13.704641342163086, -36306.94921875),
+            FarmPart = CFrame.new(1103.792236328125, 13.841465950012207, -36305.859375)
+        },
+        {
+            Name = "223 | Kawaii Dreamland",
+            TeleportPart = CFrame.new(1198.7998046875, 13.698640823364258, -36306.94921875),
+            FarmPart = CFrame.new(1265.292236328125, 13.841465950012207, -36305.859375)
+        },
+        {
+            Name = "224 | Kawaii Temple",
+            TeleportPart = CFrame.new(1360.7998046875, 13.286641120910645, -36306.94921875),
+            FarmPart = CFrame.new(1427.292236328125, 13.841465950012207, -36305.859375)
+        },
+        {
+            Name = "225 | Grassy Plains",
+            TeleportPart = CFrame.new(713.1748046875, 13.715641021728516, -24950.107421875),
+            FarmPart = CFrame.new(779.667236328125, 13.841465950012207, -24949.017578125)
+        },
+        {
+            Name = "226 | Rocky Ridge",
+            TeleportPart = CFrame.new(875.4248046875, 13.696640968322754, -24950.107421875),
+            FarmPart = CFrame.new(941.917236328125, 13.841465950012207, -24949.017578125)
+        },
+        {
+            Name = "227 | Crystal Lake",
+            TeleportPart = CFrame.new(1037.2998046875, 13.704641342163086, -24950.107421875),
+            FarmPart = CFrame.new(1103.792236328125, 13.841465950012207, -24949.017578125)
+        },
+        {
+            Name = "228 | Electro Forge",
+            TeleportPart = CFrame.new(1198.7998046875, 13.698640823364258, -24950.107421875),
+            FarmPart = CFrame.new(1265.292236328125, 13.841465950012207, -24949.017578125)
+        },
+        {
+            Name = "229 | Elemental Realm",
+            TeleportPart = CFrame.new(1360.7998046875, 13.286641120910645, -24950.107421875),
+            FarmPart = CFrame.new(1427.292236328125, 13.841465950012207, -24949.017578125)
+        },
+        {
+            Name = "230 | Elysium Fields",
+            TeleportPart = CFrame.new(5390.375, 13.715641021728516, -20570.408203125),
+            FarmPart = CFrame.new(5456.8671875, 13.841465950012207, -20569.31640625)
+        },
+        {
+            Name = "231 | Ocean Paradise",
+            TeleportPart = CFrame.new(5739.6044921875, 98.89356231689453, -20570.408203125),
+            FarmPart = CFrame.new(5806.0966796875, 99.03839111328125, -20569.31640625)
+        },
+        {
+            Name = "232 | Lost Library",
+            TeleportPart = CFrame.new(6089.61767578125, 194.95469665527344, -20570.408203125),
+            FarmPart = CFrame.new(6156.1103515625, 195.0915069580078, -20569.31640625)
+        },
+        {
+            Name = "233 | Nebula Forest",
+            TeleportPart = CFrame.new(6539.01123046875, 313.6986389160156, -20570.408203125),
+            FarmPart = CFrame.new(6605.5029296875, 313.8414611816406, -20569.31640625)
+        },        
+        {
+            Name = "234 | Aether Colosseum",
+            TeleportPart = CFrame.new(6920.125, 413.28662109375, -20570.408203125),
+            FarmPart = CFrame.new(6986.6171875, 413.8414611816406, -20569.31640625)
+        },
+    },
+    Eggs = {
+        "213 | Rusty Egg",
+        "214 | Striped Egg",
+        "215 | Cinderblocks Egg",
+        "216 | Lootbag Egg",
+        "217 | Summer Beach Egg",
+        "218 | Summer Floatie Egg",
+        "219 | Summer Melon Egg",
+        "220 | Summer Beachball Egg",
+        "221 | Summer Sun Egg",
+        "222 | Hacker Circuit Egg",
+        "223 | Hacker Wireframe Egg",
+        "224 | Hacker Crystal Egg",
+        "225 | Hacker Electric Egg",
+        "226 | Hacker Secure Egg",
+        "227 | Growing Egg",
+        "228 | Sturdy Egg",
+        "229 | Established Egg",
+        "230 | Elegant Egg",
+        "231 | Luxe Egg",
+        "232 | Sakura Blossom Egg",
+        "233 | Dream Egg",
+        "234 | Moonlight Egg",
+        "235 | Strawberry Egg",
+        "236 | Kawaii Egg",
+        "237 | Grass Type Egg",
+        "238 | Rock Type Egg",
+        "239 | Water Type Egg",
+        "240 | Electric Type Egg",
+        "241 | Fire Type Egg",
+        "242 | Elysium Egg",
+        "243 | Paradise Egg",
+        "244 | Lost Library Egg",
+        "245 | Nebula Egg",
+        "246 | Colosseum Egg",
+    },
+    Minigames = {},
+    VendingMachines = {
+        {
+            Name = "OPPotionsVendingMachine2",
+            Location = "204 | Prison HQ"
+        },
+        {
+            Name = "RareEnchantsVendingMachine2",
+            Location = "208 | Jungle Island"
+        },
+        {
+            Name = "BundleVendingMachine3",
+            Location = "214 | Hacker Mainframe"
+        },
+        {
+            Name = "FruitVendingMachine4",
+            Location = "224 | Kawaii Temple"
+        },
+        {
+            Name = "PotionVendingMachine6",
+            Location = "228 | Electro Forge"
+        },
+    },
+    Rewards = {
+        {
+            Name = "DailyEnchants5",
+            Location = "202 | Prison Cafeteria"
+        },
+        {
+            Name = "DailyDiamonds7",
+            Location = "205 | Beach Island"
+        },
+        {
+            Name = "DailyItems5",
+            Location = "206 | Ocean Island"
+        },
+        {
+            Name = "DailyPotions6",
+            Location = "212 | Hacker Cave"
+        },
+        {
+            Name = "DailyEnchants6",
+            Location = "222 | Kawaii Grove"
+        },
+        {
+            Name = "DailyDiamonds8",
+            Location = "227 | Crystal Lake"
+        },
+        {
+            Name = "DailyItems6",
+            Location = "231 | Ocean Paradise"
+        },
+    },
+    OtherMachines = {
+        {
+            Name = "SuperMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "RainbowMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "GoldMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "IndexMerchant",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "PetIndexMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "MailboxMachine",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "SpinnyWheel",
+            Location = "200 | Void Spawn"
+        },
+        {
+            Name = "SummerGiftMachine",
+            Location = "205 | Beach Island"
+        },
+        {
+            Name = "BoostExchangeMachine",
+            Location = "217 | Silver City"
+        },
+        {
+            Name = "RaffleMerchant",
+            Location = "218 | Golden Metropolis"
+        },
+        {
+            Name = "RebirthShrine",
+            Location = "219 | Diamond Mega City"
+        },
+        {
+            Name = "ElementalQuest",
+            Location = "229 | Elemental Realm"
+        },
+        {
+            Name = "PetCubeStore",
+            Location = "225 | Grassy Plains"
+        },
+        {
+            Name = "WingsUpgradesMachine",
+            Location = "230 | Elysium Fields"
+        },
+    }
+}
